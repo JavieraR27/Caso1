23c76606a9a4fc036c2846484e1d8b2d5ba60fad
danvillar-sketch <dan.villar@duocuc.cl>
2026-07-03 01:29:31 -0400

    Update OpenApiConfig.java

--- administrador/src/main/java/com/example/administrador/config/OpenApiConfig.java
+++ administrador/src/main/java/com/example/administrador/config/OpenApiConfig.java
@@ -6,10 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
-/**
- * Información OpenAPI 3 del servicio (springdoc). Swagger UI en
- * /swagger-ui/index.html y JSON en /v3/api-docs.
- */
 @Configuration
 public class OpenApiConfig {
 
