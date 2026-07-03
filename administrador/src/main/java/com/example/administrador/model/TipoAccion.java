b880916f0d01d4ae9ceb2acd97b915ec1d9b4bda
danvillar-sketch <dan.villar@duocuc.cl>
2026-07-03 01:35:18 -0400

    Update TipoAccion.java

--- administrador/src/main/java/com/example/administrador/model/TipoAccion.java
+++ administrador/src/main/java/com/example/administrador/model/TipoAccion.java
@@ -1,8 +1,6 @@
 package com.example.administrador.model;
 
-/**
- * Tipo de acción administrativa registrada en la auditoría.
- */
+
 public enum TipoAccion {
     APROBAR_PROVEEDOR,
     RECHAZAR_PROVEEDOR,
